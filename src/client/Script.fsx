dd5da761a597737cfa5cddf36044ef17c89c379f
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-16 20:56:15 +0100

    small changes

--- src/client/Script.fsx
+++
@@ -1,4 +0,0 @@
-#load "client.fs"
-open client
-
-// Define your library scripting code here
--- src/client/client.fs
+++ src/client/client.fs
@@ -1,4 +1,11 @@
-namespace client
+module Client
+open System
+open Fable.Core
+open Fable.Import
+module R = Fable.Helpers.React
+module P = Fable.Helpers.React.Props
 
-type client() = 
-    member this.X = "F#"
+Node.require.Invoke("core-js") |> ignore
+Node.require.Invoke("bootstrap/dist/css/bootstrap.css") |> ignore
+
+ReactDom.render(R.h1[][unbox "Hello World"], Browser.document.getElementById("container")) |> ignore
