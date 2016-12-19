61d9c8b3281ab12994cc1335db7a5c23e7dac35c
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-19 16:40:20 +0100

    add httpSource to mdoel

--- src/data/model.ts
+++ src/data/model.ts
@@ -1,11 +1,11 @@
 import {Actions} from "./global";
-import {Response} from "@cycle/http";
+import {Response, HTTPSource} from "@cycle/http";
 import {Result, Score, Sentiment} from "./score";
 /**
  * Created by dominik.kotecki on 19-12-2016.
  */
 
 
-function model(actions : Actions): null {
+function model(actions : Actions, http: HTTPSource): null {
     return null;
 }
