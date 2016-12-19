b44f44486d141ba329db314d04084a7e4fe795cf
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-19 16:46:06 +0100

    small changes

--- src/actions/seacrhTweet.ts
+++ src/actions/seacrhTweet.ts
@@ -1,8 +1,4 @@
-import Cycle from '@cycle/xstream-run';
-import xs from 'xstream';
 import debounce from 'xstream/extra/debounce';
-import { div, label, input, hr, ul, li, a, makeDOMDriver } from '@cycle/dom';
-import { makeHTTPDriver } from '@cycle/http';
 import { DOMSource } from "@cycle/dom/xstream-typings";
 import {PortalUrl} from "../data/consts";
 
--- src/data/model.ts
+++ src/data/model.ts
@@ -1,11 +1,18 @@
 import {Actions} from "./global";
 import {Response, HTTPSource} from "@cycle/http";
 import {Result, Score, Sentiment} from "./score";
+import {LOADING} from "./consts";
 /**
  * Created by dominik.kotecki on 19-12-2016.
  */
 
 
 function model(actions : Actions, http: HTTPSource): null {
-    return null;
+    return http.select("sentiment")
+        .flatten()
+        .map((res: Response) => {
+            const body = res.body as Result<Score>;
+            return body;
+        })
+        .startWith(LOADING);
 }
