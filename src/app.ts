839747ee518a32cc6186d31f873cdb0a65feabc3
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-19 13:29:37 +0100

    small changes

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import search from './actions/seacrhTweet';
-import searchBox from './views/searchBox';
-import { source } from '@cycle/dom/lib';
-import { ISinks, ISources } from './data/global';
-import { HTTPSource, makeHTTPDriver } from '@cycle/http/lib';
-import { Stream } from 'xstream';
+import search from "./actions/seacrhTweet";
+import searchBox from "./views/searchBox";
+import { source } from "@cycle/dom/lib";
+import { ISinks, ISources } from "./data/global";
+import { HTTPSource, makeHTTPDriver } from "@cycle/http/lib";
+import { Stream } from "xstream";
 import { div, label, input, hr, h1, makeDOMDriver, VNode } from "@cycle/dom";
 import { DOMSource } from "@cycle/dom/xstream-typings";
 import { run } from "@cycle/xstream-run";
-import debounce from 'xstream/extra/debounce';
-import * as tslib from 'tslib';
+import debounce from "xstream/extra/debounce";
+import * as tslib from "tslib";
 
 function main(sources: ISources): ISinks {
     const dom = sources.dom;
     const http = sources.http;
     return {
         dom: searchBox(http),
-        http: search(dom) 
+        http: search(dom),
     };
 }
 
