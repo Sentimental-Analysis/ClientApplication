b5d6c4aad94dec59230b727e8c27b9b234594d42
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-19 15:39:36 +0100

    small changes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,8 @@ import searchBox from "./views/searchBox";
 import { source } from "@cycle/dom/lib";
 import { ISinks, ISources } from "./data/global";
 import { HTTPSource, makeHTTPDriver } from "@cycle/http/lib";
-import { Stream } from "xstream";
 import { div, label, input, hr, h1, makeDOMDriver, VNode } from "@cycle/dom";
-import { DOMSource } from "@cycle/dom/xstream-typings";
 import { run } from "@cycle/xstream-run";
-import debounce from "xstream/extra/debounce";
 import * as tslib from "tslib";
 
 function main(sources: ISources): ISinks {
--- src/views/searchBox.ts
+++ src/views/searchBox.ts
@@ -1,16 +1,10 @@
-import { opinionToWord } from '../utils/opinios';
-import { h1, p, span } from '@cycle/dom/lib';
-import { KeyWord, Result, Score, Sentiment } from '../data/score';
-import { Response } from '@cycle/http/lib/interfaces';
-import { HTTPSource } from '@cycle/http/lib';
-import Cycle from '@cycle/xstream-run';
-import xs from 'xstream';
-import debounce from 'xstream/extra/debounce';
-import { div, label, input, hr, ul, li, a, makeDOMDriver } from '@cycle/dom';
-import { makeHTTPDriver } from '@cycle/http';
-import { DOMSource } from "@cycle/dom/xstream-typings";
-import { PortalUrl } from "../data/consts";
-import { List } from 'immutable';
+import { opinionToWord } from "../utils/opinios";
+import { h1, p, span } from "@cycle/dom/lib";
+import {  Result, Score, Sentiment } from "../data/score";
+import { Response } from "@cycle/http/lib/interfaces";
+import { HTTPSource } from "@cycle/http/lib";
+import { div, input, hr, ul, li } from "@cycle/dom";
+import { List } from "immutable";
 
 function searchBox(source: HTTPSource) {
     return source
@@ -26,10 +20,10 @@ function searchBox(source: HTTPSource) {
                 div(".row", [
                     div(".col-lg-12.nopadding", [
                         div(".input-group.input-group-lg", [
-                            input('#tweet .tweet.input-lg.form-control', { attrs: { type: 'text' } }),
+                            input("#tweet .tweet.input-lg.form-control", { attrs: { type: "text" } }),
                             span(".input-group-btn", [
-                                 input('.tweet .btn.btn-default.btn-lg', { attrs: { type: 'submit' } })
-                            ])                         
+                                input(".tweet .btn.btn-default.btn-lg", { attrs: { type: "submit" } })
+                            ])
                         ])
                     ])
                 ]),
@@ -38,8 +32,8 @@ function searchBox(source: HTTPSource) {
                     h1(`Opinia dla klucza ${result.value.key} to ${opinionToWord(result.value.sentiment)}`),
                     h1(`Ilość negatywnych opini to: ${result.value.negativeTweetsQuantity}`),
                     h1(`Ilość pozytywnych opini to: ${result.value.positiveTweetsQuantity}`),
-                    ul('.keywords', List.of(...result.value.keyWords).take(10).map(keyword =>
-                        li('.keyword', [
+                    ul(".keywords", List.of(...result.value.keyWords).take(10).map(keyword =>
+                        li(".keyword", [
                             p(`${keyword.key} | ${keyword.quantity}`)
                         ])
                     ).toArray())
