d712228e02eb2b98f4ac49898dc28c65474565fb
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-20 10:09:15 +0100

    add opinionBox

--- src/views/opinionBox.ts
+++ src/views/opinionBox.ts
@@ -1,9 +1,24 @@
-import { h1 } from "@cycle/dom/lib";
+import renderOpinionCountBox from './opinionCountBox';
+import { div, h1 } from "@cycle/dom/lib";
 import { opinionToWord } from "../utils/opinios";
 import { Score, Sentiment } from "../data/score";
 
 function renderOpinionBox(score: Score) {
-    return h1(`Opinia dla klucza ${score.key} to ${opinionToWord(score.sentiment)}`);
+    return div(".score", [
+        div(".details", [
+            div(".row.text", [
+                div(".col-md-4.well.nopadding", [
+                   renderOpinionCountBox("pozytywnych", score.positiveTweetsQuantity),
+                ]),
+                div(".col-md-4.well.nopadding", [
+                    h1(`Opinia dla klucza ${score.key} to ${opinionToWord(score.sentiment)}`),
+                ]),
+                div(".col-md-4.well.nopadding", [
+                    renderOpinionCountBox("negatywnych", score.negativeTweetsQuantity),
+                ]),
+            ]),
+        ]),
+    ]);
 };
 
 export default renderOpinionBox;
