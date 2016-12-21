9f4fde7327f12f36a7d01ffbb1e9298e7b69ced0
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-21 15:12:30 +0100

    add some interfaces

--- src/utils/heatmap.ts
+++ src/utils/heatmap.ts
@@ -6,5 +6,29 @@ export interface Point {
 }
 
 export interface Bounds {
-  contains: (latLng: Localization) => boolean;
+    contains: (latLng: Localization) => boolean;
+}
+
+export interface Pane {
+  appendChild: (element: Object) => void;
+}
+
+
+export interface Panes {
+  overlayPane: Pane;
+}
+
+export interface Map {
+    layerPointToLatLng: (lngLat: Point) => Localization;
+    latLngToLayerPoint: (lngLat: Localization) => Point;
+    on: (event: string, handler: () => void) => void;
+    getBounds: () => Bounds;
+    getPanes: () => Panes;
+    invalidateSize: () => void;
+    options: {};
+}
+
+export interface LeafletZoomEvent {
+  zoom: number;
+  center: Object;
 }
